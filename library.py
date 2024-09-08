877fbc423ef1331df099dda0812ce9a50e4093c2
Siddharth Dodia <siddharthdodia@siddharths-air.lan>
2024-09-08 11:23:17 -0500

    Initial commit

---
+++ library.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from tkinter import ttk
+
+class LibraryManagementSystem:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Library Management System')
+        self.root.geometry('1550x956+0+0')
+
+        lbltitle = Label(self.root, text='LIBRARY MANAGEMENT SYSTEM', bg= 'powder blue', fg='green', bd=20, relief= RIDGE, font= ("times new roman", 50, 'bold'), padx = 2, pady = 6)
+        lbltitle.pack(side= TOP, fill= X)
+
+        frame = Frame(self.root, bd=12, relief= RIDGE, padx = 20, bg = "powder blue")
+        frame.place(x=0, y=130, width= 1530, height= 400)
+        
+        # DataFrameLeft
+        DataFrameLeft = LabelFrame(frame, text='Library Member Info', bg= 'powder blue', fg='green', bd=12, relief= RIDGE, font= ("times new roman", 12, 'bold'), padx = 2, pady = 6)
+        DataFrameLeft.place(x=0, y=5, width= 900, height= 350)
+
+        lblMember = Label(DataFrameLeft, bg= "powder blue" ,text= "Member Type", font= ("times new roman", 15, "bold"), padx = 2, pady= 6)
+        lblMember.grid(row= 0, column= 0, sticky= W)
+
+        comMember = ttk.Combobox(DataFrameLeft, font= ("times new roman", 15, "bold"), width= 27, state= "readonly")
+        comMember['value'] = ("Admin Staff", "Student", "Lecturer")
+        comMember.grid(row=0, column= 1)
+
+        lblPRN_No = Label(DataFrameLeft, bg= "powder blue" ,text= "PRN No.", font= ("times new roman", 15, "bold"), padx = 2, pady= 6)
+        lblPRN_No.grid(row= 1, column= 0, sticky= W)
+        txtPRN_No = Entry(DataFrameLeft, font= ("times new roman", 15, "bold"),  width= 29)
+        txtPRN_No.grid(row=1, column=1)
+
+        lblTitle = Label(DataFrameLeft, bg= "powder blue" ,text= "ID No.", font= ("times new roman", 15, "bold"), padx = 2, pady= 6)
+        lblTitle.grid(row= 2, column= 0, sticky= W)
+        txtTitle = Entry(DataFrameLeft, font= ("times new roman", 15, "bold"),  width= 29)
+        txtTitle.grid(row=2, column=1)
+
+        lblFirstName = Label(DataFrameLeft, bg= "powder blue" ,text= "First Name", font= ("times new roman", 15, "bold"), padx = 2, pady= 6)
+        lblFirstName.grid(row= 3, column= 0, sticky= W)
+        txtFirstName= Entry(DataFrameLeft, font= ("times new roman", 15, "bold"),  width= 29)
+        txtFirstName.grid(row=3, column=1)
+
+        lblLastName = Label(DataFrameLeft, bg= "powder blue" ,text= "Last Name", font= ("times new roman", 15, "bold"), padx = 2, pady= 6)
+        lblLastName.grid(row= 4, column= 0, sticky= W)
+        txtLastName= Entry(DataFrameLeft, font= ("times new roman", 15, "bold"),  width= 29)
+        txtLastName.grid(row=4, column=1)
+
+        DataFrameRight = LabelFrame(frame, text='Book Details', bg= 'powder blue', fg='green', bd=12, relief= RIDGE, font= ("times new roman", 12, 'bold'), padx = 2, pady = 6)
+        DataFrameRight.place(x=910, y=5, width= 540, height= 350)
+
+        # Frame for buttons
+        FrameButton = Frame(self.root, bd=12, relief= RIDGE, padx = 20, bg = "powder blue")
+        FrameButton.place(x=0, y=530, width= 1530, height= 70)
+
+        # Information Frame
+        # Frame for buttons
+        FrameDetails = Frame(self.root, bd=12, relief= RIDGE, padx = 20, bg = "powder blue")
+        FrameDetails.place(x=0, y=600, width= 1530, height= 195)
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = LibraryManagementSystem(root)
+    root.mainloop()
